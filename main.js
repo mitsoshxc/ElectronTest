8b5d39ba5f82b78b703eb1d2a1109d0adb09904c
mitsoshxc <mitsoshxc@gmail.com>
2017-08-05 20:33:03 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800,
+        frame: false,
+        resize: false
+    });
+
+    mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+});
