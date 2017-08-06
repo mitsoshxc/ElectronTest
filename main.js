ddcf98d1e6b4e15265451cd3df0691ea21cc90ad
mitsoshxc <mitsoshxc@gmail.com>
2017-08-06 13:20:22 +0300

    overwrite makis

--- main.js
+++ main.js
@@ -10,8 +10,19 @@ app.on('ready', function() {
         height: 600,
         width: 800,
         frame: false,
-        resize: false
+        resize: false,
     });
 
     mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
 });
+
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+//Create an event handler:
+var myEventHandler = function () {
+  console.log('I hear a scream!');
+}
+
+//Assign the event handler to an event:
+eventEmitter.on('scream', myEventHandler);
